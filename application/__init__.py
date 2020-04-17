29af1037d6d0c128128a66bcdd212fe879e5dfed
Danilo Moura <danilolmoura@gmail.com>
2020-04-17 03:21:21 -0300

    Add first version of JWT autheticator

--- application/__init__.py
+++ application/__init__.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt import JWT
+from werkzeug.security import safe_str_cmp
 
 from config import config_by_name
 
@@ -9,25 +11,41 @@ db = SQLAlchemy()
 
 from application import db
 
+from application.models import User
+
 def create_app(config_name):
-	from flask_potion import Api
+    from flask_potion import Api
 
-	app = Flask(__name__, instance_relative_config=True)
-	app.config.from_object(config_by_name[config_name])
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(config_by_name[config_name])
 
-	app.logger.info('Connecting database')
-	db.init_app(app)
+    app.logger.info('Connecting database')
+    db.init_app(app)
 
-	app.logger.info('creating Flask-Potion Apis')
-	from application import apis
-	api = Api(app, prefix='/api/v1', title='Backend API')
-	apis.create_api(api)
+    app.logger.info('Setting authenticator')
+    def authenticate(email, password):
+        import pdb
+        pdb.set_trace()
+        user = User.query.filter_by(
+            email=email
+        ).first()
 
-	app.logger.info('Defining default URL')
-	@app.route('/')
-	def index():
-		return "Welcome to Backend API!"
+        if user and safe_str_cmp(
+                user.password.encode('utf-8'),
+                password.encode('utf-8')):
+            return user
 
-	app.logger.info('Finished initialization')
+    def identity(payload):
+        user_id = payload['identity']
+        return User.query.get(user_id)
 
-	return app
+    jwt = JWT(app, authenticate, identity)
+
+    app.logger.info('creating Flask-Potion Apis')
+    from application import apis
+    api = Api(app, prefix='/api/v1', title='Backend API')
+    apis.create_api(api)
+
+    app.logger.info('Finished initialization')
+
+    return app
--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = ''
+    SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     POTION_DEFAULT_PER_PAGE = 20
     POTION_MAX_PER_PAGE = 5000
